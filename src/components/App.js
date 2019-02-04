2b5d329d67a4d42f310de33aa3b600ee44aeec4b
SarKurd <sarbast2009@hotmail.co.uk>
2019-02-04 12:04:40 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+import "./App.scss";
+
+function App() {
+  return <div id="app">Hello React</div>;
+}
+export default hot(App);
