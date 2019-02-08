272806d36b7de3e0d49da530904d3dfbc6fbc008
SarKurd <sarbast2009@hotmail.co.uk>
2019-02-08 14:03:44 +0100

    Proposal class properties fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import { hot } from "react-hot-loader/root";
 import "./App.scss";
 
-function App() {
-  return <div id="app">Hello React</div>;
+class App extends Component {
+  state = {
+    world: "World"
+  };
+  render() {
+    return <div id="app">Hello, {this.state.world}!</div>;
+  }
 }
 export default hot(App);
